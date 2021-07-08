35f9e1b34eaa979e28012e184181847603ad08b0
saibharatkumar <saibharat2997@gmail.com>
2021-07-08 17:51:53 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,23 +1,39 @@
-import json
-import requests
 from time import sleep
 from google.cloud import pubsub_v1
+from google.auth import jwt
+
+from concurrent import futures
 import os
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "master-reactor-317301-403b51f02b3c.json"
 c='San Francisco'
+openweathermap_api_key=#apikey
 li=[]
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = #private json file
+publish_futures = []
+from concurrent import futures
+def get_callback(publish_future, data):
+    def callback(publish_future):
+        try:
+            pass
+            #print(publish_future.result(timeout=60))
+        except:
+            print(f'Publishing {data} timed out.')
+    return callback
 def Publish_messages(message):
     publisher = pubsub_v1.PublisherClient()
-    project_id="master-reactor-317301"
-    topic_id="MyFirstTopic"
+    project_id=#project_id
+    topic_id=#topic_id
     topic_path = publisher.topic_path(project_id, topic_id)
     message_json = json.dumps({'data': {'message': message},})
     message_bytes = message_json.encode('utf-8')
-    publisher.publish(topic_path,message_bytes)
-
-for i in range(0,2):
-    res = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={c}&appid={openweathermap_api_key}&units=imperial')
-    li.append(res.json())
-    sleep(1)
-message=li
-Publish_messages(message)
+    publish_future = publisher.publish(topic_path,message_bytes)
+    publish_future.add_done_callback(get_callback(publish_future, message))
+    publish_futures.append(publish_future)
+    futures.wait(publish_futures, return_when=futures.ALL_COMPLETED)
+for num_messages in range(10):
+    li=[]
+    for i in range(0,10):
+        res = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={c}&appid={openweathermap_api_key}&units=imperial')
+        li.append(res.json())
+        sleep(90)
+    message=li
+    Publish_messages(message)
--- cloudFunction_to_Storage.py
+++ cloudFunction_to_Storage.py
@@ -5,13 +5,14 @@ import os
 import json
 import operator as op
 import numpy as np
+import time
 
 
 def upload_data_to_bucket(df,file_name):
     storage_client = storage.Client()
     bucket_name = #bucket_name
     bucket=storage_client.get_bucket(bucket_name)
-    new_blob=bucket.blob(f'{file_name}.csv')
+    new_blob=bucket.blob(f'{file_name}'+str(time.time())+'.csv')
     new_blob.upload_from_string(data=df.to_csv(index=False))
 
     
@@ -24,7 +25,7 @@ def hello_pubsub(event, context):
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     print(pubsub_message)
 
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = #private_key
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = #private_json_key
     # print(pubsub_message)
     df=pd.DataFrame(json.loads(pubsub_message)['data']['message'])
     # print()
